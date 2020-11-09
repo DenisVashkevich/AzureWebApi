3beb4734f2bd57bbc3f099551b49fce2af38866d
Dennis Vashkevich <vdenis.biocom@gmail.com>
2020-11-09 15:50:14 +0000

    serilog setup and initial dependancies

---
+++ AdventureWorks.API/AdventureWorks.API/Program.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace AdventureWorks.API
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			Log.Logger = new LoggerConfiguration()
+				.ReadFrom.Configuration(configuration)
+				.CreateLogger();
+
+			try
+			{
+				CreateHostBuilder(args).Build().Run();
+			}
+			catch(Exception ex)
+			{
+				Log.Fatal(ex, "Application start-up failed");
+			}
+			finally
+			{
+				Log.CloseAndFlush()
+			}
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.UseSerilog()
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+}
---
+++ AdventureWorks.API/AdventureWorks.DbModel/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AdventureWorks.DbModel
+{
+	public class Class1
+	{
+	}
+}
