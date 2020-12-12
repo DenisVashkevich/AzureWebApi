1dfaf29c03874aa39855c6bb3cf18d760972962a
Denis Vashkevich <>
2020-12-12 12:25:18 +0300

    Download limit size set to defaults

--- AdventureWorks.API/AdventureWorks.API/Controllers/DocumentsController.cs
+++ AdventureWorks.API/AdventureWorks.API/Controllers/DocumentsController.cs
@@ -22,7 +22,6 @@ namespace AdventureWorks.API.Controllers
 
 
         [HttpPost]
-        [DisableRequestSizeLimit]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult> UploadDocumentAsync (IFormFile file)
@@ -41,7 +40,6 @@ namespace AdventureWorks.API.Controllers
                     FileContent = stream,
                     ContentType = file.ContentType
                 });
-
             }
 
             return Ok(new {url =  result.AbsoluteUri});
