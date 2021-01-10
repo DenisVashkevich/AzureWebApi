bf2687611bb38659bb6095f041a72722edbf1b91
Denis Vashkevich <>
2021-01-10 17:42:29 +0300

    Function change

--- AdventureWorks.API/AdventureWorks.DocFunctions/AdventureWorksDocumentsFunction.cs
+++ AdventureWorks.API/AdventureWorks.DocFunctions/AdventureWorksDocumentsFunction.cs
@@ -6,7 +6,7 @@ namespace AdventureWorks.DocFunctions
     public static class AdventureWorksDocumentsFunction
     {
         [FunctionName("AdventureWorksDocumentsFunction")]
-        public static void Run([QueueTrigger("adv-wrks-documents-notification", Connection = "AdvWorksProductsApi:Storage:ConnectionString")] string myQueueItem, ILogger log)
+        public static void Run([QueueTrigger("adv-wrks-documents-notification")] string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
